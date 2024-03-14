fde11c7b762dd435d90c5f8d1617a57b8c97bad7
Ansari299 <thamimulansar5@gmail.com>
2024-03-15 02:22:24 +0530

    yes

--- src/components/Class.js
+++ src/components/Class.js
@@ -2,7 +2,7 @@ import React from "react";
 class Class extends React.Component {
     render(){
         return (
-            <h2>I am class components  tesr</h2>
+            <h2>I am class components  tesr jkhgkjdfhg</h2>
         )
     }
  
